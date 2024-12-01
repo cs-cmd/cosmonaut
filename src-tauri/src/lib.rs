91c6b5b01613d75e5b598caa595ac2381b392752
Ray O'Neill <rayoneill.97@gmail.com>
2024-12-01 16:24:30 +0000

    Began implementing basic file reading/writing with a static filename, basic window layout.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,55 @@
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// TODO: Have this function return an object: two attributes, a success flag and
+// an error message
+#[tauri::command]
+fn save_file(file_contents: &str) -> bool {
+    if file_contents.len() == 0 {
+        return false;
+    }
+
+    println!("Saving file...");
+
+    let mut file = match File::create("Test.txt") {
+        Err(_) => return false,
+        Ok(file) => file
+    };
+
+    return match file.write_all(file_contents.as_bytes()) {
+        Err(_) => false,
+        Ok(_) => true
+    };
+}
+
+#[tauri::command]
+fn open_file(file_name: &str) -> String {
+    let mut file = match File::open(file_name) {
+        Err(_) => return "Cannot open file".to_string(),
+        Ok(handle) => handle,
+    };
+
+    let mut contents = String::new();
+
+    match file.read_to_string(&mut contents) {
+        Err(_) => "Cannot read file contents".to_string(),
+        Ok(_) => contents
+    }
+    
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, save_file, open_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+let editor;
+let saveButton;
+let openButton;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  let greeting = await invoke("greet", { name: "John" });
+
+  console.log(greeting);
+}
+
+async function saveFile() {
+  let wasSuccessful = await invoke("save_file", { fileContents: editor.value });
+
+  console.log("File save status: " + wasSuccessful);
+}
+
+async function openFile() {
+  editor.value = await invoke("open_file", { fileName: "Test.txt" });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  editor = document.querySelector("#editor");
+  saveButton = document.querySelector("#save_file_button");
+  openButton = document.querySelector("#open_file_button");
+
+  saveButton.addEventListener("click", (e) => {
+    saveFile();
   });
+
+  openButton.addEventListener("click", (e) => {
+    openFile();
+  });
+  
 });
