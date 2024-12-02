55a8f705dde0da7cfb265537730bbdef9ba27dc9
Ray O'Neill <rayoneill.97@gmail.com>
2024-12-02 03:48:48 +0000

    Began adding Rust-based dialog

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
-use std::io::Write;
 use std::io::Read;
+use std::io::Write;
+use tauri_plugin_dialog::DialogExt;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -11,7 +12,7 @@ fn greet(name: &str) -> String {
 // TODO: Have this function return an object: two attributes, a success flag and
 // an error message
 #[tauri::command]
-fn save_file(file_contents: &str) -> bool {
+fn save_file(file_name: &str, file_contents: &str) -> bool {
     if file_contents.len() == 0 {
         return false;
     }
@@ -20,13 +21,44 @@ fn save_file(file_contents: &str) -> bool {
 
     let mut file = match File::create("Test.txt") {
         Err(_) => return false,
-        Ok(file) => file
+        Ok(file) => file,
     };
 
     return match file.write_all(file_contents.as_bytes()) {
         Err(_) => false,
-        Ok(_) => true
+        Ok(_) => true,
     };
+}
+
+#[tauri::command]
+fn save_file_via_dialog(file_contents: &str) -> bool {
+    // TODO: Call 'set_parent' here to bind the new dialog to the main window
+
+    let mut was_successful;
+
+
+    tauri::AppHandle::dialog()
+        .file()
+        .set_title("Choose a file to save to")
+        .save_file(|path| {
+            // 'path' is a file path, chosen by the user. It is an Option
+            // handl
+            if let Some(p) = path {
+                was_successful = p
+                    .as_path()
+                    .and_then(|file_path| { file_path.to_str() })
+                    .and_then(|filename| {
+                        save_file(filename, file_contents);
+                        true
+                    })
+                    .ok_or_else(false);
+            }
+            else {
+                was_successful = false;
+            }
+        });
+
+    return was_successful;
 }
 
 #[tauri::command]
@@ -39,15 +71,16 @@ fn open_file(file_name: &str) -> String {
     let mut contents = String::new();
 
     match file.read_to_string(&mut contents) {
-        Err(_) => "Cannot read file contents".to_string(),
-        Ok(_) => contents
+        Err(_) => "Cannfot read file contents".to_string(),
+        Ok(_) => contents,
     }
-    
 }
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialoig::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet, save_file, open_file])
         .run(tauri::generate_context!())
--- src/main.js
+++ src/main.js
@@ -4,27 +4,22 @@ let editor;
 let saveButton;
 let openButton;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  let greeting = await invoke("greet", { name: "John" });
-
-  console.log(greeting);
-}
-
 async function saveFile() {
-  let wasSuccessful = await invoke("save_file", { fileContents: editor.value });
-
-  console.log("File save status: " + wasSuccessful);
+  let wasSuccessful = await invoke("save_file_via_dialog", { fileContents: editor.value });
 }
 
 async function openFile() {
-  editor.value = await invoke("open_file", { fileName: "Test.txt" });
+  editor.value = await invoke("open_file_via_dialog");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   editor = document.querySelector("#editor");
   saveButton = document.querySelector("#save_file_button");
   openButton = document.querySelector("#open_file_button");
+
+
+
+
 
   saveButton.addEventListener("click", (e) => {
     saveFile();
